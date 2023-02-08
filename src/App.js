d643e86f62609d54dc3e84aeb544a8add4afdd9a
oladips <oladips200@gmail.com>
2023-02-08 11:09:33 +0100

    did cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 /** @format */
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,6 +1,5 @@
 /** @format */
 import React from "react";
-import Header from "../components/Header";
 
 function Blogs() {
   return (
